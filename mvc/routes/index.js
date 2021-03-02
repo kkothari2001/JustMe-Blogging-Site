0bda988f31cbd13cf3950a8826de2d41b6b7d569
Kush Kothari <kushkothari2001@gmail.com>
2021-03-03 05:19:32 +0530

    Initial Commit

---
+++ mvc/routes/index.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var router = express.Router();
+
+const indexCtrl = require("../controllers/index");
+
+router.get("/", indexCtrl.getHomePage);
+router.get("/posts/:postid", indexCtrl.getBlogPost);
+
+router.get("/contact", indexCtrl.getContact);
+router.get("/about", indexCtrl.getAbout);
+
+router.get("/filter", indexCtrl.getFilteredList);
+
+router.get("/404", indexCtrl.get404);
+router.get("*", indexCtrl.redirect404);
+
+module.exports = router;
